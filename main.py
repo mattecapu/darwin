dbc3dca794bc70af32eaa6b76af02b64fd1b216f
Matteo Capucci <mattecapu@live.it>
2015-05-30 10:20:23 +0200

    added food placing

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import rnn
 import numpy as np
 
+# parameters
 TERRAIN_SIDE = 1000
 POPULATION_SIZE = 100
-INPUTS = 17
-HIDDENS = 60
-OUTPUTS = 5
+FOOD_LOCATIONS = 20
+INPUTS = 5
+HIDDENS = 20
+OUTPUTS = 2
+
+# constants
+FOOD = -1
 
 # a 1000x1000 grid containing informations about
 # food and individuals locations
 terrain = [[0] * TERRAIN_SIDE] * TERRAIN_SIDE
+
+# place food sources in random points on the terrain
+food_locations = [np.floor(np.random.rand(2) * TERRAIN_SIDE) for x in range(0, FOOD_LOCATIONS)]
+for x in food_locations:
+	terrain[x[0]][x[1]] = FOOD
 
 # populate our world!
 population = [rnn.RNN() for x in range(0, POPULATION_SIZE)]
