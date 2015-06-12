fc5b55c76d2d979f3a6a794cb51a174f11c21413
Matteo Capucci <mattecapu@live.it>
2015-06-12 11:14:44 +0200

    food locations are now changed every iteration to avoid overfitting

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ ALG_ITERATIONS = 10000
 POPULATION_SIZE = 32 # 64
 # a lower value should improve the
 # signal-to-noise ratio in the vision system
-FOOD_SPREADING = 16
+FOOD_DISTANCE = 24
 # fitness accuracy
 FOOD_LOCATIONS = 8
 FITNESS_COMPUTING_ITERATIONS = 32
@@ -22,11 +22,13 @@ def further(p):
 	if abs(p[1]) < 0.5: p[1] += 0.5
 	return p
 
-# place food sources in random points on the terrain
-food_locations = np.random.rand(FOOD_LOCATIONS, 2) * 2 - 1
-food_locations = [further(x) * FOOD_SPREADING for x in food_locations]
-food_distances_sq = [x[0] ** 2 + x[1] ** 2 for x in food_locations]
-food_max_distance = np.sqrt(np.amax(food_distances_sq))
+# place food sources at random points
+# along a circumference with radius FOOD_DISTANCE
+food_locations_x = np.random.rand(1, FOOD_LOCATIONS * ALG_ITERATIONS) * 2 - 1
+food_locations = np.vstack([
+	food_locations_x,
+	np.random.choice([+1, -1], FOOD_LOCATIONS * ALG_ITERATIONS) * np.sqrt(1 - food_locations_x ** 2)
+]).T * FOOD_DISTANCE
 
 # let's populate our world!
 population = [individual.create() for x in xrange(POPULATION_SIZE)]
@@ -37,18 +39,13 @@ print "simulation", RUN_PREFIX
 print ALG_ITERATIONS, " iterations (" + str(ALG_ITERATIONS * POPULATION_SIZE * FOOD_LOCATIONS * FITNESS_COMPUTING_ITERATIONS), "cycles)"
 print "\n"
 
-print "food at"
-for f in food_locations: print f
-print "\n"
-
 for epoch in xrange(ALG_ITERATIONS):
 	for nn in population:
 		nn.fitness = 0
-		for food_i in xrange(FOOD_LOCATIONS):
-			food = food_locations[food_i]
+		for f in xrange(FOOD_LOCATIONS):
+			food = food_locations[epoch * FOOD_LOCATIONS + f]
 			for i in xrange(FITNESS_COMPUTING_ITERATIONS): nn.tick(nn.visibility(food))
-			final_food_distance = np.linalg.norm((nn.position[0] - food[0], nn.position[1] - food[1]))
-			nn.fitness += (final_food_distance * food_max_distance) / food_distances_sq[food_i]
+			nn.fitness += np.linalg.norm((nn.position[0] - food[0], nn.position[1] - food[1])) / FOOD_DISTANCE
 			nn.reset()
 		nn.fitness /= FOOD_LOCATIONS
 
