0a89729a5a7206c0465c0dcc885f43aafa49f950
Matteo Capucci <mattecapu@live.it>
2015-06-13 11:10:30 +0200

    works, show title and axes labels

--- display_fitness.m
+++ display_fitness.m
@@ -1,12 +1,14 @@
 function display_fitness(run)
 	data = load(["data/fitness/run" int2str(run) ".m"]);
+	figure("visible", "off")
 	clf()
 	newplot()
-	ylim([0 max(data(:, 2)) + 0.6])
+	ylim([0 1])
+	scatter(data(:, 1), data(:, 2), 2, "b", ".")
 	title("population fitness")
 	ylabel("fitness")
 	xlabel("iteration")
-	scatter(data(:, 1), data(:, 2), 2, "b", ".")
 	mkdir("data/plots", int2str(run));
-	print(["data/plots/" int2str(run) "/fitness.png"], "-Ggs.cmd")
+	set(gcf(), "paperposition", [0 0 11 6])
+	print(["data/plots/" int2str(run) "/fitness.png"], "-r100", "-Ggs.cmd")
 end
