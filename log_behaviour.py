cleaned up data folder structure, fixed useless conversion to radians in display_behaviour, fixed missing int conversion in log_behaviour, fixed wrong food placing in log_behaviour

log_behaviour.py
@@ -1,5 +1,7 @@
 import sys
+import os
 import numpy as np
+
 from individual import individual
 from serialize import dedump
 from load_config import load_config
@@ -9,12 +11,14 @@ config = load_config()
 # get parameters of the simulation
 RUN_PREFIX = sys.argv[1]
 EPOCH = sys.argv[2]
-ITERATIONS = config["behaviour_logging_iterations"] if len(sys.argv) < 4 else sys.argv[3]
+ITERATIONS = config["behaviour_logging_iterations"] if len(sys.argv) < 4 else int(sys.argv[3])
 
-food = [np.sqrt(2) * config["food_distance"]] * 2
+food = [config["food_distance"] / np.sqrt(2)] * 2
 # load genes
 subject = dedump(RUN_PREFIX, EPOCH)
-log = open("data/behaviours/run" + str(RUN_PREFIX) + "_iter" + str(EPOCH) + ".dat", "w")
+if not os.path.exists("data/behaviours/" + str(RUN_PREFIX)):
+	os.makedirs("data/behaviours/" + str(RUN_PREFIX))
+log = open("data/behaviours/" + str(RUN_PREFIX) + "/" + str(EPOCH) + ".dat", "w")
 
 for epoch in xrange(ITERATIONS):
 	# data about the subject

serialize.py
@@ -1,8 +1,12 @@
 import numpy as np
+import os
+
 from individual import individual
 
 def filename(run, epoch):
-	return "data/weights/run" + str(run) + "_iter" + str(epoch) + ".dat"
+	if not os.path.exists("data/weights/" + str(run)):
+		os.makedirs("data/weights/" + str(run))
+	return "data/weights/" + str(run) + "/" + str(epoch) + ".dat"
 
 def dump(run, epoch, indiv):
 	np.savetxt(filename(run, epoch), np.concatenate((indiv.f_chrom[:, 0, :], indiv.m_chrom[:, 0, :])))