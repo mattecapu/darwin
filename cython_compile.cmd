af0616cb0857ad6dc2ed34f9b0b5a9d0c49e1ce1
Matteo Capucci <mattecapu@live.it>
2015-06-15 21:37:09 +0200

    removed duplicated compilation, added optimization option

--- cython_compile.cmd
+++ cython_compile.cmd
@@ -1,4 +1,3 @@
 @echo off
-call "D:\bin\Cython-0.22\bin\cython" "%1.pyx" -2
-call gcc -c "%1.c" -I"C:\Program Files (x86)\Python27\include"
-call gcc -shared "%1.c" -I"C:\Program Files (x86)\Python27\include" -L"C:\Program Files (x86)\Python27\libs" -lpython27 -o "%1.pyd"
+call "D:\bin\Cython-0.22\bin\cython" "%1.pyx" -a
+call gcc -shared "%1.c" -I"C:\Program Files (x86)\Python27\include" -I"C:\Program Files (x86)\Python27\Lib\site-packages\numpy\core\include" -L"C:\Program Files (x86)\Python27\libs" -lpython27 -Ofast -o "%1.pyd"
