99e11c1ef83cb14da061d55d3738b2b9926f93d2
Matteo Capucci <mattecapu@live.it>
2015-06-16 15:33:34 +0200

    exploring higher optimization options on both GCC and ICC

--- cython_compile.cmd
+++ cython_compile.cmd
@@ -1,3 +1,3 @@
 @echo off
 call "D:\bin\Cython-0.22\bin\cython" "%1.pyx" -a
-call gcc -shared "%1.c" -I"C:\Program Files (x86)\Python27\include" -I"C:\Program Files (x86)\Python27\Lib\site-packages\numpy\core\include" -L"C:\Program Files (x86)\Python27\libs" -lpython27 -Ofast -o "%1.pyd"
+call gcc -shared "%1.c" -I"C:\Program Files (x86)\Python27\include" -I"C:\Program Files (x86)\Python27\Lib\site-packages\numpy\core\include" -L"C:\Program Files (x86)\Python27\libs" -lpython27 -O3 -ffast-math -fno-signed-zeros -fno-trapping-math -fassociative-math -fwhole-program -flto -march=native -o "%1.pyd"
