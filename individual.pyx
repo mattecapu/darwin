fd965d9bbd5415088866d82195bf8fd9bd1486de
Matteo Capucci <mattecapu@live.it>
2015-06-16 22:28:09 +0200

    ported more code to cython

--- individual.pyx
+++ individual.pyx
@@ -7,61 +7,43 @@ from rnn cimport RNN
 from rnn import RNN
 
 cdef extern from "math.h":
-	float sin(float)
-	float cos(float)
-	float abs(float)
-	float atan2(float, float)
-	float asin(float)
-	float sqrt(float)
-
-from load_config import load_config
-config = load_config()
+	float sin(float) nogil
+	float cos(float) nogil
+	float abs(float) nogil
+	float atan2(float, float) nogil
+	float asin(float) nogil
+	float sqrt(float) nogil
+	float pow(float, int) nogil
 
 # morphology
-cdef int EYES = config["eyes"]
+DEF EYES = 5
 
 # rnn layers
-cdef int INPUT_LAYER_SIZE = EYES
-cdef int HIDDEN_LAYERS = config["hidden_layers"]
-cdef int HIDDEN_LAYERS_SIZE = config["hidden_layers_size"]
-cdef int OUTPUT_LAYER_SIZE = config["output_layer_size"]
+DEF INPUT_LAYER_SIZE = EYES
+DEF HIDDEN_LAYERS = 2
+DEF HIDDEN_LAYERS_SIZE = 10
+DEF OUTPUT_LAYER_SIZE = 3
 # number of chromosome pairs
-cdef int CHROMOSOME_LENGTH = HIDDEN_LAYERS_SIZE
-cdef int APLOID_NUMBER = INPUT_LAYER_SIZE + HIDDEN_LAYERS_SIZE * (2 * HIDDEN_LAYERS - 1) + OUTPUT_LAYER_SIZE
+DEF CHROMOSOME_LENGTH = HIDDEN_LAYERS_SIZE
+DEF APLOID_NUMBER = INPUT_LAYER_SIZE + HIDDEN_LAYERS_SIZE * (2 * HIDDEN_LAYERS - 1) + OUTPUT_LAYER_SIZE
 # probability of mutation events during mating
-cdef float CROSSING_OVER_RATE = config["crossing_over_rate"]
-cdef float MUTATION_RATE = config["mutation_rate"]
+DEF CROSSING_OVER_RATE = 0.5
+DEF MUTATION_RATE = 0.15
 
 # constants
 cdef float TURN = 2 * np.pi
 cdef float FIELD_OF_VIEW = TURN / (2 * EYES)
 EYES_VECTOR_SHAPE = (EYES, 1)
 cdef float[:] EYE_ANGLES = ((2 * np.arange(EYES).reshape(EYES_VECTOR_SHAPE) + 1) * FIELD_OF_VIEW / 2).astype(np.float32)[:, 0]
-cdef float LIGHT_INTENSITY = config["light_intensity"]
+DEF LIGHT_INTENSITY = 1024
 
 cdef float DEG = 180 / np.pi
 
 # get the angle of incidence of light at angle phi
-cdef float get_theta(float x, float y, float phi):
+cdef float get_theta(float x, float y, float phi) nogil:
 	return abs(atan2(x - sin(phi), y - cos(phi)) + phi)
 
 cdef class individual:
-	cdef public float position_x
-	cdef public float position_y
-	cdef public float fitness
-	cdef public float rotation
-	cdef public np.ndarray f_chrom
-	cdef public np.ndarray m_chrom
-	cdef public RNN brain
-
-	@staticmethod
-	def create():
-		# generates APLOID_NUMBER random chromosomes
-		cdef np.ndarray[np.float32_t, ndim = 2] gamete1 = np.random.rand(APLOID_NUMBER, CHROMOSOME_LENGTH).astype(np.float32)
-		cdef np.ndarray[np.float32_t, ndim = 2] gamete2 = np.random.rand(APLOID_NUMBER, CHROMOSOME_LENGTH).astype(np.float32)
-		return individual(gamete1, gamete2)
-
-	@cython.boundscheck(False)
 	def __init__(self, np.ndarray[np.float32_t, ndim = 2] _f_chrom, np.ndarray[np.float32_t, ndim = 2] _m_chrom):
 		self.fitness = 0
 		# stores the chromosomes
@@ -85,14 +67,13 @@ cdef class individual:
 		)
 		self.reset()
 
-	def reset(self):
+	cpdef reset(self):
 		self.position_x = 0
 		self.position_y = 0
 		self.rotation = TURN / 8
 		self.brain.reset()
 
-	@cython.boundscheck(False)
-	def tick(self, food_x, food_y):
+	cpdef tick(self, float food_x, float food_y):
 		# outputs are changes to position and orientation
 		cdef float delta_x, delta_y, delta_rot
 		cdef float[:] output = self.brain.forward(self.visibility(food_x, food_y))[0]
@@ -106,44 +87,45 @@ cdef class individual:
 		self.position_y += delta_x * -sin_rot + delta_y * cos_rot
 		self.rotation = (self.rotation + delta_rot * TURN) % TURN
 
-	@cython.boundscheck(False)
 	cdef np.ndarray[np.float32_t, ndim = 2] visibility(self, float food_x, float food_y):
 		# C-fast loop counter
 		cdef int i
-		# find inclination of the point (angle of the line between origin and point)
 		cdef float y_dist, x_dist
-		x_dist = food_x - self.position_x
-		y_dist = food_y - self.position_y
-		cdef float angle = atan2(y_dist, x_dist)
-		# intensity dimishes with the square of the distance... or so
-		cdef float d_squared = y_dist ** 2 + x_dist ** 2
-		cdef float intensity = LIGHT_INTENSITY / (d_squared / 2)
-		# max and min angle from which you can see 'point'
-		cdef float theta_min = asin(1 / sqrt(d_squared))
-		cdef float theta_max = TURN / 4 - 2 * theta_min
-		# rotate wrt the point inclination
-		theta_min += angle - TURN / 4
-		theta_max += angle
-		cdef np.ndarray[np.float32_t, ndim = 2] result = np.zeros(EYES_VECTOR_SHAPE, dtype = np.float32)
-		cdef float phi
-		for i in range(EYES):
-			# rotate the eyes wrt animal orientation
-			phi = EYE_ANGLES[i] - self.rotation
-			if theta_min < phi < theta_max:
-				# compute incidence on the eyes
-				result[i, 0] = intensity * sin(get_theta(food_x, food_y, phi))
+		cdef float angle, d_squared, intensity
+		cdef float theta_min, theta_max, phi
+		cdef float[EYES] result
+		with nogil:
+			# find inclination of the point (angle of the line between origin and point)
+			x_dist = food_x - self.position_x
+			y_dist = food_y - self.position_y
+			angle = atan2(y_dist, x_dist)
+			# intensity dimishes with the square of the distance... or so
+			d_squared = pow(y_dist, 2) + pow(x_dist, 2)
+			intensity =  2 * LIGHT_INTENSITY / d_squared
+			# max and min angle from which you can see 'point'
+			theta_min = asin(1 / sqrt(d_squared))
+			theta_max = TURN / 4 - 2 * theta_min
+			# rotate wrt the point inclination
+			theta_min += angle - TURN / 4
+			theta_max += angle
+			for i in range(EYES):
+				# rotate the eyes wrt animal orientation
+				phi = EYE_ANGLES[i] - self.rotation
+				if theta_min < phi < theta_max:
+					# compute incidence on the eyes
+					result[i] = intensity * sin(get_theta(food_x, food_y, phi))
+				else:
+					result[i] = 0
 
-		return result
+		return np.asarray(<np.float32_t[:]>result).reshape(EYES_VECTOR_SHAPE)
 
-	@cython.boundscheck(False)
-	def gamete(self):
+	cdef np.ndarray[np.float32_t, ndim = 2] gamete(self):
 		cdef np.ndarray[np.float32_t, ndim = 2] gamete
 		# C-fast loop counter
 		cdef int i
 		# random variables
 		cdef float m_or_f
 		cdef int split_loc, mutation_loc
-
 		# allocate the gamete
 		gamete = np.empty((APLOID_NUMBER, CHROMOSOME_LENGTH), dtype = np.float32)
 
@@ -169,5 +151,11 @@ cdef class individual:
 
 		return gamete
 
-	def mate(self, partner):
+	cpdef individual mate(self, individual partner):
 		return individual(self.gamete(), partner.gamete())
+
+cpdef individual create_indiv():
+	# generates APLOID_NUMBER random chromosomes
+	cdef np.ndarray[np.float32_t, ndim = 2] gamete1 = np.random.rand(APLOID_NUMBER, CHROMOSOME_LENGTH).astype(np.float32)
+	cdef np.ndarray[np.float32_t, ndim = 2] gamete2 = np.random.rand(APLOID_NUMBER, CHROMOSOME_LENGTH).astype(np.float32)
+	return individual(gamete1, gamete2)
--- main.py
+++ main.py
@@ -1,116 +1,26 @@
 import numpy as np
-from multiprocessing import Pool
-import sys
 
-from individual import individual
-from serialize import dump
+import sys
+import time
+
 from load_config import load_config
+from simulation import simulation
 
 config = load_config()
-
-# parameters
 ITERATIONS = (config["iterations"] if len(sys.argv) < 2 else int(sys.argv[1])) + 1
-POPULATION_SIZE = config["population_size"]
-MATING_FRACTION = config["mating_fraction"]
-# a lower value should improve the
-# signal-to-noise ratio in the vision system
-FOOD_DISTANCE = config["food_distance"]
-# fitness accuracy
-FOOD_LOCATIONS = config["food_locations"]
-FITNESS_COMPUTING_ITERATIONS = config["fitness_computing_iterations"]
+# flag to disable logging for quick test runs
+DRY_RUN = len(sys.argv) > 2 and sys.argv[2] == "dry"
 
-# number of weights dumps to take
-DUMPS = config["dumps"]
+# id of this simulation
+RUN_PREFIX = str(np.random.randint(2 ** 12))
 
-def calc_fitness((indiv, food_locs)):
-	fitness = 0
-	for (food_x, food_y) in food_locs:
-		for i in xrange(FITNESS_COMPUTING_ITERATIONS): indiv.tick(food_x, food_y)
-		# the fitness is computed as the fraction of the distance traveled towards the food
-		# but because fitness should be maximized and not minimized, we use the shifted reciprocal
-		# (1 / (1 + x)) of this quantity, where the shift avoid a vertical asymptote at 0,
-		# and the reciprocal invert the trend from descending to ascending
-		fitness += 1 / (1 + np.sqrt((indiv.position_x - food_x) ** 2 + (indiv.position_y - food_y) ** 2))
-		indiv.reset()
-	return fitness / FOOD_LOCATIONS
+print "## simulation", RUN_PREFIX
+print "##", ITERATIONS, " iterations\n"
 
-def sex((partner_1, partner_2)):
-	return partner_1.mate(partner_2)
+start = time.clock()
+simulation(ITERATIONS, RUN_PREFIX, DRY_RUN)
+end = time.clock()
 
-if __name__ == "__main__":
-	# flag to disable logging for quick test runs
-	DRY_RUN = len(sys.argv) > 2 and sys.argv[2] == "dry"
-	# flag to disable parallelization for better testing
-	# (error reporting from parallel process is quite shitty)
-	PARALLEL = not (len(sys.argv) > 3 and sys.argv[3] == "nopar")
-
-	if not DRY_RUN:
-		# id of this simulation
-		RUN_PREFIX = str(np.random.randint(2 ** 12))
-
-	# place food sources at random points
-	# along a circumference with radius FOOD_DISTANCE
-	food_samples = FOOD_LOCATIONS * ITERATIONS
-	food_locations_x = np.cos(np.pi/4 + ((np.random.rand(1, food_samples).astype(np.float32) * 2 - 1) * np.arange(1, food_samples + 1) / (food_samples)))
-	food_locations = zip(
-		(food_locations_x * FOOD_DISTANCE)[0],
-		(np.random.choice([+1, -1], food_samples) * np.sqrt(1 - food_locations_x ** 2) * FOOD_DISTANCE).astype(np.float32)[0]
-	)
-
-	# let's populate our world!
-	population = [individual.create() for x in xrange(POPULATION_SIZE)]
-
-	if not DRY_RUN:
-		history = open("data/fitness/run" + str(RUN_PREFIX) + ".m", "w")
-		print "simulation", RUN_PREFIX
-		print ITERATIONS, " iterations (" + str(ITERATIONS * POPULATION_SIZE * FOOD_LOCATIONS * FITNESS_COMPUTING_ITERATIONS), "cycles)"
-		print "\n"
-
-	if PARALLEL:
-		# parallel processes pool
-		pool = Pool(processes = 4)
-		parallelize = pool.map
-	else:
-		parallelize = map
-
-	for epoch in xrange(ITERATIONS):
-		# compute fitness of every individual
-		food_locs = food_locations[(epoch * FOOD_LOCATIONS):((epoch + 1)* FOOD_LOCATIONS)]
-		fitnesses = parallelize(calc_fitness, [(population[i], food_locs) for i in xrange(POPULATION_SIZE)])
-		for i in xrange(POPULATION_SIZE):
-			population[i].fitness = fitnesses[i]
-
-		# sort by best fitness (descendent order)
-		population.sort(key = lambda x: -x.fitness)
-
-		if not DRY_RUN:
-			# log the best fitness
-			history.write(str(epoch) + ' ' + str(population[0].fitness) + '\n')
-			# dump weights of the best
-			if (epoch % (ITERATIONS / DUMPS)) == 0 or epoch == 0:
-				dump(RUN_PREFIX, epoch, population[0])
-				# log to console
-				print epoch, "-> dump at fitness", population[0].fitness
-			elif epoch % (ITERATIONS / (DUMPS * 10)) == 0:
-				print epoch, "fitness is", population[0].fitness
-
-		# skew to increase mating success for high fitness individuals
-		skewed_fitnesses = np.array(fitnesses[:POPULATION_SIZE * MATING_FRACTION]) ** 10
-		skewed_prob = skewed_fitnesses / sum(skewed_fitnesses)
-		matings = zip(*[np.random.choice(POPULATION_SIZE * MATING_FRACTION, POPULATION_SIZE, p = skewed_prob) for i in xrange(2)])
-		# mates
-		children = parallelize(sex, [(population[p1], population[p2]) for (p1, p2) in matings])
-
-		# move on!
-		population = children
-
-	if PARALLEL:
-		# close the child processes
-		pool.close()
-		pool.join()
-
-	if not DRY_RUN:
-		# don't leave an opened file pointer!
-		history.close()
-
-		print "\n", "simulation", RUN_PREFIX, "ended"
+print "\n## simulation", RUN_PREFIX, "ended"
+print "## total time:", (end - start), "seconds"
+print "## time per iteration:", (end - start) / ITERATIONS, "seconds"
