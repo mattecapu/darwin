0bacbd49dc0fadc71e2701ea7b5d6bb2e71ccb89
Matteo Capucci <mattecapu@live.it>
2015-06-30 13:44:51 +0200

    moved python code & compiling scripts in a separate folder

--- cy_icc_opt.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-call cython_transpile %1
-call icc -shared "%1.c" -I"C:\Program Files (x86)\Python27\include" -I"C:\Program Files (x86)\Python27\Lib\site-packages\numpy\core\include" -L"C:\Program Files (x86)\Python27\libs" -lpython27 -O3 -march=native -pc64 -fast-transcendentals -unroll-aggressive -parallel -fp-model fast -ftz -ipo -std=c99 -o "%1.pyd"
